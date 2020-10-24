5258b5cee5b5fde551d5089443b15239452f22c4
OscarBest96 <oscarbest5@yahoo.co.uk>
2020-10-24 02:02:29 +0100

    added IR control, half works, half doesn't, need to inspect..

--- buggy_control_with_servo_ultrasound/mega_servo_ultrasound_motors.ino/mega_servo_ultrasound_motors.ino.ino
+++ buggy_control_with_servo_ultrasound/mega_servo_ultrasound_motors.ino/mega_servo_ultrasound_motors.ino.ino
@@ -1,85 +1,56 @@
 #include <Servo.h>
 #include <SimpleTimer.h>
+#include "commands.h"
 #include <NewPing.h>
 #include <LiquidCrystal_I2C.h>
+#include <IRremote.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
- 
 #define TRIGGER_PIN 46
 #define ECHO_PIN 44
 #define MAX_DISTANCE 1000
-#define SPEED 150
-#define TURNSPEED 255
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 SimpleTimer t;
 SimpleTimer t1;
 SimpleTimer t2;
 
-
 Servo myservo;  // create servo object to control a servo
 
-
+const int RECV_PIN = 31;
 int pos = 30;    // variable to store the servo position
 int counter = 1;
-int distance;
 int water;
+int distance;
+
 bool autonomous = false;
-bool printLock = true;
-bool writeLock = true;
 bool startSensors = false;
+bool writeLock = true;
+bool printLock = true;
 
-
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+IRrecv irrecv(RECV_PIN);
+decode_results results;
 
 
 void getPing(){
-  if(startSensors==true){
+if(startSensors==true){
   distance = sonar.ping_cm();
-  
-      if(writeLock==true){
-      writeLock=false;
-      Serial1.print('A');
-      Serial1.write(distance);
-      writeLock=true;
+  Serial.println(distance);
+  if(writeLock==true){
+  writeLock=false;
+  Serial1.print('A');
+  Serial1.write(distance);
+  writeLock=true;
       }
   }
-  
   if(autonomous==true){ 
-      if(distance<=40){
-        
-        if(pos>=30 && pos<=75){
-          //motor A: FORWARD
-            digitalWrite(12, LOW); //Establishes forward direction of Channel A
-            digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-            analogWrite(3, TURNSPEED);   //Spins the motor on Channel A at full speed
-            //motor B: BREAK
-            digitalWrite(13, LOW); //Establishes forward direction of Channel A
-            digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-            analogWrite(11, TURNSPEED);   //Spins the motor on Channel A at full speed
-          }
-            
-           if(pos>=75 && pos<=120){
-            digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-            digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-            analogWrite(3, TURNSPEED);   //Spins the motor on Channel A at full speed
-            //motor B: FORWARD
-            digitalWrite(13, HIGH); //Establishes forward direction of Channel A
-            digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-            analogWrite(11, TURNSPEED);   //Spins the motor on Channel A at full speed
-            }
-          }
-     else {
-            digitalWrite(12, LOW); //Establishes forward direction of Channel A
-            digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-            analogWrite(3, SPEED);   //Spins the motor on Channel A at full speed
-            //motor B: FORWARD
-            digitalWrite(13, HIGH); //Establishes forward direction of Channel A
-            digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-            analogWrite(11, SPEED);   //Spins the motor on Channel A at full speed            
-            }
-            
-  }
+      if(distance<=30){
+        if(pos>=30 && pos<=75){left();}
+        if(pos>=75 && pos<=120){right();}
+        }
+        else {forward();}
+     }
 }
 
 
@@ -101,34 +72,41 @@ if(water>255){water=255;}
       Serial1.write(water);
       writeLock=true;
     }
-}
+  }
 if(water>100){
   autonomous = false;
-  digitalWrite(9, HIGH);   //Disengage the Brake for Channel A
-  digitalWrite(8, HIGH);   //Disengage the Brake for Channel A
+  brakes();
   }
 }
 
+
+void WATER_WARNING(){
+if (water>100 && printLock==true){
+lcd.print("WARNING: WATER");
+printLock=false;
+}
+else if (water<100) {
+  if(printLock==false){
+  lcd.clear();
+  }
+  printLock=true;
+}
+}
+
 void setup() {
-  
- 
   t.setInterval(25, sweep_servo);
-  t1.setInterval(200, getPing);
-  t2.setInterval(400, water_sensor);
-
-
+  t1.setInterval(100, getPing);
+  t2.setInterval(300, water_sensor);
   Serial.begin(9600);
   Serial1.begin(9600);
-  //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
-  //Setup Channel B
-  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
-  pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+  motor_setup();
   myservo.attach(7);  // attaches the servo on pin 9 to the servo object
   lcd.backlight();
   lcd.init();
 }
+
 
 
 
@@ -137,84 +115,88 @@ void loop() {
   t1.run();
   t2.run();
 
-  if (water>100 && printLock==true){
-  lcd.print("WARNING: WATER");
-  printLock=false;
-  }
-  else if (water<100) {
-    if(printLock==false){
-    lcd.clear();
-    }
-    printLock=true;
-  }
- 
- if (Serial1.available()) {  //When data is available in the buffer..
-    char command = Serial1.read();  //store value in 'C'
-     
-          switch(command){
-            
-                case '1':
-                startSensors=!startSensors;
-                break;
-                
-                case 'w':
-                //motor A: FORWARD
-                digitalWrite(12, LOW); //Establishes forward direction of Channel A
-                digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-                analogWrite(3, SPEED);   //Spins the motor on Channel A at full speed
-                //motor B: FORWARD
-                digitalWrite(13, HIGH); //Establishes forward direction of Channel A
-                digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-                analogWrite(11, SPEED);   //Spins the motor on Channel A at full speed
-                break;
-        
-                case 'a':
-                //motor A: FORWARD
-                digitalWrite(12, LOW); //Establishes forward direction of Channel A
-                digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-                analogWrite(3, TURNSPEED);   //Spins the motor on Channel A at full speed
-                //motor B: BREAK
-                digitalWrite(13, LOW); //Establishes forward direction of Channel A
-                digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-                analogWrite(11, TURNSPEED);   //Spins the motor on Channel A at full speed
-                break;
-        
-                case 'd':
-                //motor A: B
-                digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-                digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-                analogWrite(3, TURNSPEED);   //Spins the motor on Channel A at full speed
-                //motor B: FORWARD
-                digitalWrite(13, HIGH); //Establishes forward direction of Channel A
-                digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-                analogWrite(11, TURNSPEED);   //Spins the motor on Channel A at full speed
-                break;
-        
-                case 's':
-                //motor A: BACKWARD
-                digitalWrite(12, HIGH); //Establishes forward direction of Channel A
-                digitalWrite(9, LOW);   //Disengage the Brake for Channel A
-                analogWrite(3, SPEED);   //Spins the motor on Channel A at full speed
-                //motor B: BACKWARD
-                digitalWrite(13, LOW); //Establishes forward direction of Channel A
-                digitalWrite(8, LOW);   //Disengage the Brake for Channel A
-                analogWrite(11, SPEED);   //Spins the motor on Channel A at full speed
-                break;
-              
-                //BOTH BRAKE
-                case 'b':
-                digitalWrite(9, HIGH);   //Disengage the Brake for Channel A
-                digitalWrite(8, HIGH);   //Disengage the Brake for Channel A
-                break;  
-
-                case 'n':
-                autonomous = !autonomous;
-                break;
-
-                
-                }
-
-       }
-  
-
+ WATER_WARNING();
+ PC_COMMAND();
+ IR_COMMAND();
 }
+
+
+
+
+
+ void PC_COMMAND(){
+   if (Serial1.available()) {  //When data is available in the buffer..
+    char command = Serial1.read();  //store value in 'C'
+    switch(command){
+            
+          case '1':
+          startSensors=!startSensors;
+          break;
+          
+          case 'w':
+          forward();
+          break;
+  
+          case 'a':
+          left();
+          break;
+  
+          case 'd':
+          right();
+          break;
+  
+          case 's':
+          backward();
+          break;
+        
+          case 'b':
+          brakes();
+          break;  
+
+          case 'n':
+          startSensors=true;
+          autonomous = !autonomous;
+          break;
+      }
+     }
+ }
+
+ void IR_COMMAND(){
+  
+  if (irrecv.decode(&results)){
+    Serial.println(results.value, HEX);
+    switch(results.value){
+  
+      case 0xFFA25D:
+      startSensors=!startSensors;
+      break;
+      
+      case 0xFF18E7:  
+      forward();
+      break;
+
+      case 0xFF10EF:
+      left();
+      break;
+
+      case 0xFF5AA5:
+      right();
+      break;
+
+      case 0xFF4AB5:
+      backward();
+      break;
+    
+      case 0xFF38C7:
+      brakes();
+      break;  
+
+      case 0xFF6897:
+      startSensors=true;
+      autonomous = !autonomous;
+      break;
+      }
+      
+  irrecv.resume();
+  }
+ }
