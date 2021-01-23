35781a02f086fc532db881ff7a88f3ac3b3af634
OscarBest96 <oscarbest5@yahoo.co.uk>
2021-01-23 19:59:44 +0000

    commented, most recent code for ESP32 & Arduino

--- buggy_control_with_servo_ultrasound/PC-esp32-mega_motor_commands/PC-esp32-mega_motor_commands.ino
+++ buggy_control_with_servo_ultrasound/PC-esp32-mega_motor_commands/PC-esp32-mega_motor_commands.ino
@@ -1,47 +1,48 @@
-#include <BlynkSimpleEsp32.h>
-#include <SimpleTimer.h>
-#include "WiFi.h" // ESP32 WiFi include
-#include "wifiConfig.h" // My WiFi configuration. 
-#include "DHT.h"
-#include <IRremote.h>
+#include <BlynkSimpleEsp32.h> //lib for Blynk app
+#include <SimpleTimer.h>      //lib for timer 
+#include "WiFi.h"             //ESP32 WiFi include
+#include "wifiConfig.h"       //my WiFi configuration 
+#include "DHT.h"              //lib for DHT sensor
+#include <IRremote.h>         //lib for IR receiver
 
 
-#define BLYNK_PRINT Serial
-#define DHTPIN 23        // what digital pin we're connected to
-#define DHTTYPE DHT11     // DHT11
-DHT dht(DHTPIN, DHTTYPE);
+#define BLYNK_PRINT Serial //define serial comunication for Blynk
+#define DHTPIN 23          // what digital pin DHT sensor is connected to
+#define DHTTYPE DHT11      // DHT11
+DHT dht(DHTPIN, DHTTYPE);  //create object dht
 
 
-//192.168.0.77  <--- IP
+//192.168.0.77  <--- IP adress for esp32 on my network 
 
-#define RXD2 16 //these are the ports responisble for serial port 2
+#define RXD2 16 //these are the ports responsible for serial port 2
 #define TXD2 17
  
-WiFiServer wifiServer(80);
-WiFiClient client;
+WiFiServer wifiServer(80); //server object listening to port 80
+WiFiClient client;         //client object created 
 
-SimpleTimer T;
-SimpleTimer H;
+SimpleTimer T;     //init timer T
+SimpleTimer H;     //init timer H
 
-bool startSensors = false;
-const int RECV_PIN = 18;
-char command;
+bool startSensors = false;  //init sensors as OFF
+const int RECV_PIN = 18;    //receive pin for IR
+char command;               //init variable command
 
-IRrecv irrecv(RECV_PIN);
-decode_results results;
+IRrecv irrecv(RECV_PIN);  //object irrecv at pin 18 
+decode_results results;   //object for IR results
 
-
+//function to measure temperature
 void read_temperature(){
- if(startSensors==true){
-  int temp = dht.readTemperature();
-  Serial.println(temp);
-  Blynk.virtualWrite(V1,temp);
+
+ if(startSensors==true){            //if sensors are turned on
+  int temp = dht.readTemperature(); //take temperature reading and save in variable 'temp'
+  Blynk.virtualWrite(V1,temp);      //write temp value to Blynk app - pin V1
   }
 }
+//function to measure humidity
 void read_humidity(){
-  if(startSensors==true){
-  int humidity = dht.readHumidity();
-  Blynk.virtualWrite(V2,humidity); 
+  if(startSensors==true){            //if sensors are turned on
+  int humidity = dht.readHumidity(); //take humidity reading and save in variable 'humidity'
+  Blynk.virtualWrite(V2,humidity);   //write humidity value to blynk app - pin V2
   }
 }
 
@@ -51,116 +52,125 @@ void read_humidity(){
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(9600); //start serial with baud rate 9600
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2); // starts serial port 2, wired connection to arduino mega.
-  irrecv.enableIRIn();
+  T.setInterval(5000, read_temperature); // call read_temperature every 5 seconds
+  H.setInterval(5000, read_humidity);    // call read_humidity every 5 seconds
+  irrecv.enableIRIn();      //init IR reciever input 
   irrecv.blink13(true);
   delay(1000);
-  WiFi.begin(ssid, pass);
+  WiFi.begin(ssid, pass); //init WiFi connection using login provided in wifiConfig.h file
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Connecting to WiFi..");  //print this every second while connecting
   }
-  Serial.println("Connected to the WiFi network");
-  Serial.println(WiFi.localIP());
+  Serial.println("Connected to the WiFi network"); //print this once connection is made
+  Serial.println(WiFi.localIP()); //print the IP address for esp32 server
  
-  wifiServer.begin();
-  Blynk.begin(auth, ssid, pass);
-  dht.begin();
-  T.setInterval(5000, read_temperature);
-  H.setInterval(5000, read_humidity);
+  wifiServer.begin(); //start server
+  Blynk.begin(auth, ssid, pass); //initialise Blynk connection to esp32
+  dht.begin(); //begin dht sensor measure 
 }
  
 void loop() {
-  client = wifiServer.available();
-  Blynk.run();
-  T.run();
+  client = wifiServer.available(); //store wifiServer availability into client object
+  Blynk.run(); //run Blynk
+  T.run();     //run timers T,H
   H.run();
-  IR_COMMAND();
-if (Serial2.available()) {
-serial2Event();
+  IR_COMMAND();      //run function IR_COMMAND
+  if(command=='1'){  //if command veriable has value 1
+  startSensors=!startSensors; //toggle startSensors - ON/OFF
+  if (Serial2.available()) {  //if data is at rx of serial2
+  serial2Event();             // run function serial2Event
+
 }
-if(command=='1'){
-startSensors=!startSensors;
 }
  
-  if (client) {
- //Serial.println ("YOOOO!");
+  if (client) {  //if wifi server is available 
 
 
-      while (client.connected()) {
-        
-             IR_COMMAND();
+      while (client.connected()) { //when connected to the server
+
+             T.run(); //run timers T,H
+             H.run();
+             IR_COMMAND(); //run function IR_command
                   
-             if (client.available()) {
-             clientEvent();
+             if (client.available()) { //if data is available for client to read
+             clientEvent();            //run function clientEvent
+             }
+
+             if (Serial2.available()) { //if data is received at rx of serial2
+             serial2Event();            //run function serial2Event
              }
 
        }
       
-      client.stop();
-      Serial.println("Client disconnected");
+      client.stop();                         //disconnect from server
+      Serial.println("Client disconnected"); //print disconnect message to terminal
       }
 }
 
+//function to read data from client and store into variable 'command'
 void clientEvent(){
-command = client.read();
-Serial2.write(command); //write data to serial every second
+command = client.read(); //data from client stored in command variable
+Serial2.write(command); //write command variable data to tx line of serial2
+if(command=='1'){       //if command received ==1,  
+  startSensors=!startSensors; //toggle sensors on esp32
+}
 }
 
+//function takes unique char, followed by data & stores into corresponding variables
 void serial2Event(){
-char ID = Serial2.read();  //store value in 'C'
-if (ID=='A'){
- //   if(Serial2.available()){
-     int distance = Serial2.read();
-     Blynk.virtualWrite(V0,distance);
+char ID = Serial2.read();  //read data at rx or serial2 and store into variable ID
+if (ID=='A'){              //if ID == A
+     int distance = Serial2.read();   //store the next byte received into variable 'distance'
+     Blynk.virtualWrite(V0,distance); //write distance value to Blynk app pin V0 
  //   }
    }
-    else if(ID=='B'){
- //    if(Serial2.available()){
-      int water = Serial2.read();
-     // Serial.println(water);
-      Blynk.virtualWrite(V3,water);
+    else if(ID=='B'){      //if ID == B
+      int water = Serial2.read(); //store the next byte into variable 'water'
+      Blynk.virtualWrite(V3,water); //write water value to Blynk app pin V3
       
   //   }
     }
 }
 
+//Function reads result from IR transmitter, compares the result, and writes to serial2
+//values written to serial2 will be used by the arduino in switch-case to drive the motors
  void IR_COMMAND(){
   
-  if (irrecv.decode(&results)){
-     Serial.println(results.value, HEX);
+  if (irrecv.decode(&results)){          //if IR code is received 
      switch(results.value){
   
-      case 0xFFA25D:
+      case 0xFFA25D:              //code 0xFFA25D (1 on IR remote) writes '1' to serial2 and stores value '1' in variable command
       Serial2.write('1');
       command = '1';
       break;
       
-      case 0xFF18E7:  
+      case 0xFF18E7:              //code 0xFF18E7 (up arrow on IR remote) writes 'w' to serial2
       Serial2.write('w');
       break;
 
-      case 0xFF10EF:
+      case 0xFF10EF:              //code 0xFF10EF (left arrow on IR remote) writes 'a' to serial2
       Serial2.write('a');
       break;
 
-      case 0xFF5AA5:
+      case 0xFF5AA5:              //code 0xFF5AA5 (right arrow on IR remote) writes 'd' to serial2
       Serial2.write('d');
       break;
 
-      case 0xFF4AB5:
+      case 0xFF4AB5:              //code 0xFF4AB5 (down arrow on IR remote) writes 's' to serial2
       Serial2.write('s');
       break;
     
-      case 0xFF38C7:
+      case 0xFF38C7:              //code 0xFF38C7 (OK on IR remote) writes 'b' to serial2
       Serial2.write('b');
       break;  
 
-      case 0xFF6897:
+      case 0xFF6897:              //code 0xFF6897 (* on IR remote) writes 'n' to serial2
       Serial2.write('n');
       break;
       }
-    irrecv.resume();
+    irrecv.resume();              //exit result decoding and resume function
   }
  }
--- buggy_control_with_servo_ultrasound/mega_servo_ultrasound_motors.ino/mega_servo_ultrasound_motors.ino.ino
+++ buggy_control_with_servo_ultrasound/mega_servo_ultrasound_motors.ino/mega_servo_ultrasound_motors.ino.ino
@@ -1,159 +1,152 @@
-#include <Servo.h>
-#include <SimpleTimer.h>
-#include "commands.h"
-#include <NewPing.h>
-#include <LiquidCrystal_I2C.h>
-//#include <IRremote.h>
+#include <Servo.h> //lib for servo control
+#include <SimpleTimer.h> //lib for timers
+#include "commands.h" //lib for motor control
+#include <NewPing.h>  //lib for ultrasound sensor
+#include <LiquidCrystal_I2C.h> //lib for lcd display
 
-#define TRIGGER_PIN 46
-#define ECHO_PIN 44
-#define MAX_DISTANCE 1000
+#define TRIGGER_PIN 46 //init trigger pin for ultrasound
+#define ECHO_PIN 44    //init echo pin for ultrasound
+#define MAX_DISTANCE 1000 // max distance of 1000cm
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);  //create ultrasound object 'sonar'
+LiquidCrystal_I2C lcd(0x27, 16, 2);   //init lcd display
 
-SimpleTimer t;
+SimpleTimer t;  //init timers, t, t1, t3
 SimpleTimer t1;
 SimpleTimer t2;
 
 Servo myservo;  // create servo object to control a servo
 
-//const int RECV_PIN = 31;
-int pos = 30;    // variable to store the servo position
-int counter = 1;
-int water;
-int distance;
+int pos = 30;    //variable to store the servo position
+int counter = 1; //variable to move servo
+int water;       //variable to store water value
+int distance;    //variable to store distance (cm)
 
-bool autonomous = false;
-bool startSensors = false;
-bool writeLock = true;
-bool printLock = true;
-
-//IRrecv irrecv(RECV_PIN);
-//decode_results results;
+bool autonomous = false;   //bool to control autonomous behaviour
+bool startSensors = false; //bool to start/stop sensor function
+bool writeLock = true;     //bool used as mutex lock for writing to uart data line
+bool printLock = true;     //bool used to lock printing to lcd
 
 
+
+//function to measure distance
 void getPing(){
-if(startSensors==true){
-  distance = sonar.ping_cm();
-  //Serial.println(distance);
-  if(writeLock==true){
-  writeLock=false;
-  Serial1.print('A');
-  Serial1.write(distance);
-  writeLock=true;
+if(startSensors==true){       //execute function if sensors have started
+  distance = sonar.ping_cm(); //store distance reading in variable 'distance'
+  if(writeLock==true){        //if writelock is available, execute code
+  writeLock=false;            //take lock
+  Serial1.print('A');         //print char 'A' to uart
+  Serial1.write(distance);    //write distance value to uart
+  writeLock=true;             //release lock
       }
   }
-  if(autonomous==true){ 
-      if(distance<=30){
-        if(pos>=30 && pos<=75){left();}
-        if(pos>=75 && pos<=120){right();}
+  if(autonomous==true){       //if autonomous is enabled, execute code
+      if(distance<=40){       //minimum distance before obstacle avoidance
+        if(pos>=30 && pos<=75){left();}   //whilst ultrasound is facing right, buggy turns left 
+        if(pos>=75 && pos<=120){right();} //whilst ultrasound is facing left, buggy turns right
         }
-        else {forward();}
+        else {forward();}     //while distance is above 40cm, continue forward
      }
 }
 
-
-void sweep_servo() {
-counter=counter+5;
-float rad = counter*(3.14159/180);
-pos = 45*sin(rad)+75;
-myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//function to rotate servo with mounted ultrasound
+void sweep_servo() {     
+counter=counter+5;      //increase variable counter by 5
+float rad = counter*(3.14159/180);  //convert counter value to radian and store in variable 'rad'
+pos = 45*sin(rad)+75;   //sin function to cycle position between 75 - 120, creating sweep motion
+myservo.write(pos);     // tell servo to go to position in variable 'pos'
 }
 
-
+//function to measure water value
 void water_sensor(){
-if(startSensors==true){
-water = analogRead(A5);
-if(water>255){water=255;}
-    if(writeLock==true){
-      writeLock=false;
-      Serial1.print('B');
-      Serial1.write(water);
-      writeLock=true;
+if(startSensors==true){ //execute function if sensors have started
+water = analogRead(A5); //store water value in variable 'water'
+if(water>255){water=255;}   //if water value is higher than 255 (higher than uart tranfer limit) assert value to max = 255
+    if(writeLock==true){    //if writelock is available, execute number
+      writeLock=false;      //take lock
+      Serial1.print('B');   //print char 'B' to uart
+      Serial1.write(water); //write water value to uart 
+      writeLock=true;       //release lock
     }
   }
-if(water>100){
-  autonomous = false;
-  brakes();
+if(water>100){         //if water value is above 100
+  autonomous = false;  //turn off autonomous mode
+  brakes();            //stop the buggy
   }
 }
 
-
+//function to display warning message to lcd
 void WATER_WARNING(){
-if (water>100 && printLock==true){
-lcd.print("WARNING: WATER");
-printLock=false;
+if (water>100 && printLock==true){  //if water value is above 100 and lcd is available to print to
+lcd.print("WARNING: WATER");        //print string to lcd
+printLock=false;                    //disable printing to lcd
 }
-else if (water<100) {
-  if(printLock==false){
-  lcd.clear();
+else if (water<100) {               //if water value is less than 100
+  if(printLock==false){             //if printing is disabled
+  lcd.clear();                      //clear lcd
   }
-  printLock=true;
+  printLock=true;                   //enable lcd printing
 }
 }
 
 void setup() {
-  t.setInterval(25, sweep_servo);
-  t1.setInterval(100, getPing);
-  t2.setInterval(300, water_sensor);
-  Serial.begin(9600);
-  Serial1.begin(9600);
-//  irrecv.enableIRIn();
- // irrecv.blink13(true);
-  motor_setup();
-  myservo.attach(7);  // attaches the servo on pin 9 to the servo object
-  lcd.backlight();
-  lcd.init();
+  t.setInterval(25, sweep_servo);    //timer  calls sweep_servo every 25ms
+  t1.setInterval(100, getPing);      //timer calls getPing every 100ms
+  t2.setInterval(300, water_sensor); //timer calls water_sensor every 300ms
+  Serial.begin(9600);                //init serial baud rate 9600
+  Serial1.begin(9600);               //init serial1 baud rate 9600
+  motor_setup();                     //init motors
+  myservo.attach(7);                 //attaches the servo on pin 9 to the servo object
+  lcd.backlight();                   //init lcd backlight
+  lcd.init();                        //init lcd
 }
 
 
 
 
 void loop() {
-  t.run();
+  t.run();   //start timers
   t1.run();
   t2.run();
 
- WATER_WARNING();
- COMMAND();
+ WATER_WARNING(); //call function to check water level
+ COMMAND();       //check serial rx for command data
 }
 
 
 
 
-
+//function to send commands to motors
  void COMMAND(){
-   if (Serial1.available()) {  //When data is available in the buffer..
-    char command = Serial1.read();  //store value in 'C'
-    Serial.println(command);
+   if (Serial1.available()) {       //When data is available in the buffer..
+    char command = Serial1.read();  //store value in 'command'
     switch(command){
             
-          case '1':
+          case '1':                  //if command value = '1', toggle sensor functionality
           startSensors=!startSensors;
           break;
           
-          case 'w':  
+          case 'w':                  //if command value = 'w', move buggy forward 
           forward();
           break;
   
-          case 'a':
+          case 'a':                  //if command value = 'a', turn buggy left
           left();
           break;
   
-          case 'd':
+          case 'd':                  //if command value = 'd', turn buggy right
           right();
           break;
   
-          case 's':
+          case 's':                  //if command value = 's', move buggy backwards
           backward();
           break;
         
-          case 'b':
+          case 'b':                  //if command value = 'b', stop buggy
           brakes();
           break;  
 
-          case 'n':
+          case 'n':                  //if command value = 'n', start sensors and toggle autonomous mode
           startSensors=true;
           autonomous = !autonomous;
           break;
